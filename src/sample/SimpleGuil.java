72b6170f1d2c199e5e0f695acde9a339497cbe0c
vasekva <vahonin_vasek@mail.ru>
2019-11-21 05:34:00 +0300

    Написал несколько строчек для понимания работы программы.

---
+++ src/sample/SimpleGuil.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javax.swing.*;
+
+public class SimpleGuil {
+    public static void main (String[] args) {
+
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click me");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //строка завершения работы при закрытии окна
+
+
+        frame.getContentPane().add(button); //кнопка на окно
+
+        frame.setSize(300, 300);
+
+        frame.setVisible(true); //делаем фрейм видимым(иначе никак)
+    }
+}
