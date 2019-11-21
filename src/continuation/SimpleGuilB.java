735264bf5015893f9cf1924eb67448301bc5253d
vasekva <vahonin_vasek@mail.ru>
2019-11-21 05:45:18 +0300

    Первая реализация кнопки в GUI

---
+++ src/continuation/SimpleGuilB.java
@@ -0,0 +1,29 @@
+package continuation;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class SimpleGuilB implements ActionListener { // <--Кнопка будет передавать собитий только тем,кто реализует этот инт-йс
+    JButton button;
+
+    public static void main (String[] args) {
+        SimpleGuilB gui = new SimpleGuilB();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        button = new JButton("click me");
+
+        button.addActionListener(this); // <--регистрация кнопки(отслеживание ее событий)
+
+        frame.getContentPane().add(button); // <-добавление кнопки на панель фрейма
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // <-закрытие проги от окна
+        frame.setSize(300, 300);
+        frame.setVisible(true); // <- !!!видимость окна!!!
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        button.setText("I've been clicked!"); // реализация метода интерфейса. Фактически метод обработки событий
+    }
+}
